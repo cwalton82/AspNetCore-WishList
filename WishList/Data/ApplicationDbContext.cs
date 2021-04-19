71f3af564146bf610a075c836d8b94e225595cdf
carlwalton <Carl.Walton@jcb-finance.co.uk>
2021-04-19 20:15:09 +0100

    Create item model wit Entity

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WishList.Model;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; };
+
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ WishList/Model/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace WishList.Model
+    
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+        public Item()
+        {
+        }
+
+    }
+}
