8fd6b4fb5abf523290f02797e4edbbecb0623f6d
carlwalton <Carl.Walton@jcb-finance.co.uk>
2021-04-22 08:05:36 +0100

    Full Check in

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,11 +12,43 @@ namespace WishList.Controllers
 {
     public class ItemController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext Context)
         {
-            return View();
+            _context = Context;
         }
 
+
+        // GET: /<controller>/ Copmlete NUMBER 2
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items.Find(Id);
+            _context.Remove(item);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
     }
 }
